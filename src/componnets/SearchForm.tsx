4897c74c05edc21189749a81902e54ba89f45357
arashjp8 <arashjafarpour723@gmail.com>
2025-01-31 18:57:15 +0330

    [refactor] - better file aragement and seperate searchform

---
+++ src/componnets/SearchForm.tsx
@@ -0,0 +1,60 @@
+import { useRef } from "react";
+import { Toaster } from "sonner";
+import LoadingSpinner from "../assets/icons/LoadingSpinner";
+
+interface SearchFormProps {
+  isLoading: boolean;
+  onSearch: (movieName: string) => void;
+}
+
+export default function SearchForm({ isLoading, onSearch }: SearchFormProps) {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!ref.current) return;
+
+    const movieName = ref.current.value.trim();
+    if (!movieName) return;
+
+    onSearch(movieName);
+    ref.current.blur();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={"flex flex-col gap-4"}>
+      <input
+        ref={ref}
+        type={"text"}
+        className={
+          "w-full h-16 px-6 bg-searchbg rounded-xl border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-accent focus:border-accent transition duration-300 ease-in-out placeholder:text-icon/40"
+        }
+        placeholder={"Type a movie name..."}
+      />
+      <Toaster
+        position={"bottom-center"}
+        richColors
+        toastOptions={{
+          classNames: {
+            toast: "font-mono",
+            title: "text-lg",
+          },
+        }}
+      />
+      <button
+        type={"submit"}
+        className={
+          "flex items-center justify-center bg-accent text-background py-2 px-6 rounded-xl hover:bg-accent/80 transition cursor-pointer"
+        }
+      >
+        {isLoading ? (
+          <div className={"animate-spin"}>
+            <LoadingSpinner />
+          </div>
+        ) : (
+          "Search"
+        )}
+      </button>
+    </form>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,67 @@
-import SearchBox from "../componnets/SearchBox";
+import { useCallback, useEffect, useState } from "react";
+import { apiClient } from "../services/apiClient";
+import { SearchApiResponse } from "../types/searchApiResponse";
+import { Torrent } from "../types/torrent";
+import { toast } from "sonner";
+import SearchForm from "../componnets/SearchForm";
+import TorrentList from "../componnets/TorrentList";
 
 export default function Home() {
+  const [movieName, setMovieName] = useState<string | null>(null);
+  const [response, setResponse] = useState<SearchApiResponse | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [torrents, setTorrents] = useState<Torrent[] | null>(null);
+  const [isFading, setIsFading] = useState<boolean>(false);
+
+  const fetchMagnets = useCallback(async () => {
+    if (!movieName) return;
+    setIsLoading(true);
+    setIsFading(true);
+
+    const res = await apiClient("movie-torrent", "GET", "application/json", {
+      movie_name: movieName,
+    });
+
+    setResponse(res);
+  }, [movieName]);
+
+  useEffect(() => {
+    if (movieName) {
+      fetchMagnets();
+    }
+  }, [movieName, fetchMagnets]);
+
+  useEffect(() => {
+    if (response) {
+      setTimeout(() => {
+        setTorrents(response.torrents);
+        setIsFading(false);
+      }, 300);
+      setIsLoading(false);
+    }
+  }, [response]);
+
+  const handleSearch = (newMovieName: string) => {
+    if (movieName === newMovieName) {
+      return toast.error("Already Searched!");
+    }
+    setMovieName(newMovieName);
+  };
+
   return (
-    <>
-      <SearchBox />
-    </>
+    <div
+      className={
+        "w-full lg:w-[50%] max-w-[1000px] p-6 text-xl lg:text-3xl text-icon flex flex-col gap-4"
+      }
+    >
+      <h2 className={"font-bold"}>Search For The Movie:</h2>
+      <SearchForm isLoading={isLoading} onSearch={handleSearch} />
+
+      <div
+        className={`transition-opacity duration-300 ${isFading ? "opacity-0" : "opacity-100"}`}
+      >
+        {torrents && <TorrentList torrents={torrents} />}
+      </div>
+    </div>
   );
 }
